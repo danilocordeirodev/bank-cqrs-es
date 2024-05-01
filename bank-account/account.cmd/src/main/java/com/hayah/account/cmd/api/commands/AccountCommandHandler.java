cf6107dc8e92d23f3c738baca17427eeaa26714c
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-05-01 16:07:37 -0300

    add command handler

---
+++ bank-account/account.cmd/src/main/java/com/hayah/account/cmd/api/commands/AccountCommandHandler.java
@@ -0,0 +1,42 @@
+package com.hayah.account.cmd.api.commands;
+
+import com.hayah.account.cmd.domain.AccountAggregate;
+import com.hayah.cqrs.core.handlers.EventSourcingHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountCommandHandler implements CommandHandler {
+    @Autowired
+    private EventSourcingHandler<AccountAggregate> eventSourcingHandler;
+
+    @Override
+    public void handle(OpenAccountCommand command) {
+        var aggregate = new AccountAggregate(command);
+        eventSourcingHandler.save(aggregate);
+    }
+
+    @Override
+    public void handle(DepositFundsCommand command) {
+        var aggregate = eventSourcingHandler.getById(command.getId());
+        aggregate.depositFunds(command.getAmount());
+        eventSourcingHandler.save(aggregate);
+    }
+
+    @Override
+    public void handle(WithdrawFundsCommand command) {
+        var aggregate = eventSourcingHandler.getById(command.getId());
+        if(command.getAmount() > aggregate.getBalance()) {
+            throw new IllegalStateException("Withdrawal declined, insuficient funds!");
+        }
+        aggregate.withdrawFunds(command.getAmount());
+        eventSourcingHandler.save(aggregate);
+    }
+
+    @Override
+    public void handle(CloseAccountCommand command) {
+        var aggregate = eventSourcingHandler.getById(command.getId());
+        aggregate.closeAccount();
+        eventSourcingHandler.save(aggregate);
+    }
+}
